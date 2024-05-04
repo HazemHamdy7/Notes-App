23f40a285f4b6bb333074036c8c5d13263eca967
Hazem Hamdy7 <hazemhamdy1215@gmail.com>
2024-05-03 22:49:09 -0700

    Add color to AddNoteBottomSheet container And add border to model sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,16 +9,18 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const NotesViewBody(),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
                 context: context,
                 builder: (context) {
                   return const AddNoteBottomSheet();
                 });
           },
           child: const Icon(Icons.add)),
+      body: const NotesViewBody(),
     );
   }
 }
