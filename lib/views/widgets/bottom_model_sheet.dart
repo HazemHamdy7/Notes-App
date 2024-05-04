5a0c06ae38a202df30f165f8f490807231b560a6
Hazem Hamdy7 <hazemhamdy1215@gmail.com>
2024-05-03 23:04:55 -0700

    Add custom text field widget and primary color constant
    - Added a custom text field widget with styling based on primary color constant.
    - Created a primary color constant for the app theme.

--- lib/views/widgets/bottom_model_sheet.dart
+++ lib/views/widgets/bottom_model_sheet.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'custom_text_field.dart';
+
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(height: 32),
+          CustomTextField(),
+        ],
+      ),
     );
   }
 }
