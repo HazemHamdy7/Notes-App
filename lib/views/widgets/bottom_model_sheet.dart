0158f73fb48d19ea88fb4dd2ff0b6b980e0574d7
Hazem Hamdy7 <hazemhamdy1215@gmail.com>
2024-05-05 15:43:17 -0700

    add AbsorbPointer  to don't take any action when the sheet is loading

--- lib/views/widgets/bottom_model_sheet.dart
+++ lib/views/widgets/bottom_model_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import '../../cubits/add_note_cubit/add_note_cubit.dart';
 import 'add_note_form.dart';
 
@@ -11,7 +10,7 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AddNoteCubit(),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+      child: BlocListener<AddNoteCubit, AddNoteState>(
         listener: (context, state) {
           if (state is AddNoteFailer) {
             debugPrint('Failed ${state.errorMessage} ');
@@ -20,14 +19,12 @@ class AddNoteBottomSheet extends StatelessWidget {
             Navigator.pop(context);
           }
         },
-        builder: (context, state) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: SingleChildScrollView(
-              child: AddNoteForm(),
-            ),
-          );
-        },
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: SingleChildScrollView(
+            child: AddNoteForm(),
+          ),
+        ),
       ),
     );
   }
